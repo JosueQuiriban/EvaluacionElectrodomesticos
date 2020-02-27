6af794fe5faabed603f57c308ca012152987e380
JosueQuiriban <josuealejandropq@gmail.com>
2020-02-27 15:07:20 -0300

    Creacion Clase Electrodomestico

--- EvaluacionElectrodomesticos/src/paquetePrueba/Electrodomestico.java
+++ EvaluacionElectrodomesticos/src/paquetePrueba/Electrodomestico.java
@@ -2,5 +2,35 @@ package paquetePrueba;
 
 public class Electrodomestico {
 	
-	//Creacion de este magnifico proyect
+	private int precioBase;
+	private String color;
+	private String consumoEnergetico;
+	private float peso;
+	
+	private final int PRECIO_BASE = 100000;
+	private final String COLOR = "blanco";
+	private final String CONSUMO_ENERGETICO = "F";
+	private final float PESO = 5;
+	
+	public Electrodomestico() {
+		this.precioBase = 0;
+		this.color = "";
+		this.consumoEnergetico = "";
+		this.peso = 0;
+	}
+	
+	public Electrodomestico(int precioBase, float peso) {
+		this();
+		this.precioBase = PRECIO_BASE;
+		this.peso = PESO;
+	}
+	
+	public Electrodomestico(int precioBase, float peso, String color, String consumoEnergetico ) {
+		this(precioBase, peso);
+		this.color = COLOR;
+		this.consumoEnergetico = CONSUMO_ENERGETICO;
+		
+	}
+
+	
 }
